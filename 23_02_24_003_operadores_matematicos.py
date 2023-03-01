b1d06560afd020fdda5227b7606c063d02ebf3f1
ingedavidddt <126051579+ingedavidddt@users.noreply.github.com>
2023-02-28 22:32:33 -0600

    capitulo 7-9

---
+++ 23_02_24_003_operadores_matematicos.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 22:14:07 2023
+
+@author: david
+"""
+
+suma = 5 + 5
+
+resta = 23 - 41
+
+mult = 3*7
+
+division = 4865/78
+
+print("5 + 5 =\t", suma, "\n23 - 41 =\t", resta, "\n3 * 7 =\t", mult, "\n4865 / 68 =\t", division)
+# para concatenar distintos tipos de datos debemos usar la coma "," para esto
+
+exponente = 4 ** 8
+# de esta forma podemos elevar un numero a una potencia
+print(exponente)
+
+print("sin truncar decimales \n4865/78 =\t", division, "\ntruncando decimales \n4865/78 =\t", round(division,4))
+# con el metodo round() podemos truncar la cantidad de decimales que decemos
