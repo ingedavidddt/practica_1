1d5333179e34221a8d7880f9af9d9a6ee9b176dd
ingedavidddt <126051579+ingedavidddt@users.noreply.github.com>
2023-03-11 23:53:32 -0600

    capitulo 2
    este capitulo trata sobre el uso básico de las variables en Python

---
+++ 23_02_27_001_Variables.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 12:04:30 2023
+
+@author: david
+"""
+
+variable = "variables" # creamos una variable de tipo string
+
+print(variable)
+
+variable = 5 # reasignamos le valor de la variable original
+
+print(variable * 5) # se mostrara el valor de de la multiplicacion de la variable en la consola
